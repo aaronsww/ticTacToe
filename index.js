2e0bd19a0c7ace6488aface05cac9c8be33c4113
Jeevan Aaron <jeevan.aaron.joseph@gmail.com>
2022-11-27 11:46:12 +0530

    feat: takes player 1 & 2 names as input

---
+++ index.js
@@ -0,0 +1,14 @@
+const Gameboard = () => {
+    // let gameBoard = ['x','p','o','x','o','x','o','x','o' ];
+    const assign = document.querySelector(".submitBtn")
+    assign.addEventListener('click', () => {
+        const player1 = document.getElementById("player1").value;
+        const player2 = document.getElementById("player2").value;
+    })
+    
+}   
+
+Gameboard()
+
+
+
