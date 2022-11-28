2858ae6be4abe8fbcd6bb3bd3953e1cfe2966051
Jeevan Aaron <jeevan.aaron.joseph@gmail.com>
2022-11-28 16:25:42 +0530

    feat: can now alternate between player X and player O

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+let flag = 0;
+
 const Gameboard = () => {
     let gameBoard = [];
     let gameControl = 0;
     let n;
-    let flag = 0;
     const assign = document.querySelector(".submitBtn")
     assign.addEventListener('click', () => {
         const player1 = document.getElementById("player1").value;
@@ -11,25 +12,30 @@ const Gameboard = () => {
     const zero = document.querySelector(".zero");
     zero.addEventListener('click', () => {
         n = 0;
-        console.log(n)
-        assignment(n,flag,gameBoard);
+        console.log(flag)
+        assignment(n,gameBoard);
+        zero.textContent = gameBoard[0]
     });
     const one = document.querySelector(".one");
     one.addEventListener('click', () => {
         n = 1;
-        console.log(n)
+        console.log(flag)
+        assignment(n,gameBoard);
+        one.textContent = gameBoard[1]
     });
     const two = document.querySelector(".two");
     two.addEventListener('click', () => {
         n = 2;
-        console.log(n)
+        console.log(flag)
+        assignment(n,gameBoard);
+        two.textContent = gameBoard[2]
     });
     
 }   
--
+
 Gameboard()
 
-function assignment(n,flag,gameBoard){
+function assignment(n,gameBoard){
     switch(n)
     {
         case 0:
@@ -37,14 +43,42 @@ function assignment(n,flag,gameBoard){
             {
                 gameBoard[0] = 'X';
                 console.log(gameBoard[0])
+                flag = 1;
             }
             else
             {
                 gameBoard[0] = 'O';
+                flag = 0;
             } break;
-        
+        case 1:
+            if(flag == 0)
+            {
+                gameBoard[1] = 'X';
+                console.log(gameBoard[1])
+                flag = 1;
+            }
+            else
+            {
+                gameBoard[1] = 'O';
+                flag = 0;
+            } break;
+        case 2:
+            if(flag == 0)
+            {
+                gameBoard[2] = 'X';
+                console.log(gameBoard[2])
+                flag = 1;
+            }
+            else
+            {
+                gameBoard[2] = 'O';
+                flag = 0;
+            } break;
     }
 }
+
+
+
 
 
 // const three = document.querySelector(".three");
