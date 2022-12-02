657697f59ae7f49cf4f9f4a4c8d3a214f1f3517e
Jeevan Aaron <jeevan.aaron.joseph@gmail.com>
2022-12-02 22:55:29 +0530

    refactor: removed switch case and now checks for current player

--- index.js
+++ index.js
@@ -1,93 +1,51 @@
-let flag = 0;
-
 const Gameboard = () => {
     let gameBoard = [];
-    let gameControl = 0;
-    let n;
     const assign = document.querySelector(".submitBtn")
     assign.addEventListener('click', () => {
         const player1 = document.getElementById("player1").value;
         const player2 = document.getElementById("player2").value;
     })  
+    let currentPlayer = player1;
     const zero = document.querySelector(".zero");
     zero.addEventListener('click', () => {
-        n = 0;
-        console.log(flag)
-        assignment(n,gameBoard);
+        console.log(currentPlayer);
+        if(currentPlayer == player1){
+            gameBoard[0] = 'X';
+            currentPlayer = player2;
+        }
+        else{
+            gameBoard[0] = 'O';
+            currentPlayer = player1;
+        }
         zero.textContent = gameBoard[0]
     });
     const one = document.querySelector(".one");
     one.addEventListener('click', () => {
-        n = 1;
-        console.log(flag)
-        assignment(n,gameBoard);
+        console.log(currentPlayer);
+        if(currentPlayer == player1){
+            gameBoard[1] = 'X';
+            currentPlayer = player2;
+        }
+        else{
+            gameBoard[1] = 'O';
+            currentPlayer = player1;
+        }
         one.textContent = gameBoard[1]
     });
     const two = document.querySelector(".two");
     two.addEventListener('click', () => {
-        n = 2;
-        console.log(flag)
-        assignment(n,gameBoard);
+        console.log(currentPlayer);
+        if(currentPlayer == player1){
+            gameBoard[2] = 'X';
+            currentPlayer = player2;
+        }
+        else{
+            gameBoard[2] = 'O';
+            currentPlayer = player1;
+        }
         two.textContent = gameBoard[2]
     });
     
 }   
 
 Gameboard()
-
-function assignment(n,gameBoard){
-    switch(n)
-    {
-        case 0:
-            if(flag == 0)
-            {
-                gameBoard[0] = 'X';
-                console.log(gameBoard[0])
-                flag = 1;
-            }
-            else
-            {
-                gameBoard[0] = 'O';
-                flag = 0;
-            } break;
-        case 1:
-            if(flag == 0)
-            {
-                gameBoard[1] = 'X';
-                console.log(gameBoard[1])
-                flag = 1;
-            }
-            else
-            {
-                gameBoard[1] = 'O';
-                flag = 0;
-            } break;
-        case 2:
-            if(flag == 0)
-            {
-                gameBoard[2] = 'X';
-                console.log(gameBoard[2])
-                flag = 1;
-            }
-            else
-            {
-                gameBoard[2] = 'O';
-                flag = 0;
-            } break;
-    }
-}
-
-
-
-
-
-// const three = document.querySelector(".three");
-// three.addEventListener('click', () => {
-//     n = 3;
-//     console.log(n)
-// });
-// const four = document.querySelector(".four");
-// four.addEventListener('click', () => {
-//     n = 4;
-//     console.log(n)
-// });
