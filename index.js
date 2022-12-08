463c77544bb659d6469e04e7d80a7a69d30dc4eb
Jeevan Aaron <jeevan.aaron.joseph@gmail.com>
2022-12-08 20:12:28 +0530

    refactor: shortened each event listener by adding function

--- index.js
+++ index.js
@@ -1,51 +1,86 @@
-const Gameboard = () => {
-    let gameBoard = [];
-    const assign = document.querySelector(".submitBtn")
-    assign.addEventListener('click', () => {
-        const player1 = document.getElementById("player1").value;
-        const player2 = document.getElementById("player2").value;
-    })  
-    let currentPlayer = player1;
-    const zero = document.querySelector(".zero");
-    zero.addEventListener('click', () => {
-        console.log(currentPlayer);
-        if(currentPlayer == player1){
-            gameBoard[0] = 'X';
-            currentPlayer = player2;
-        }
-        else{
-            gameBoard[0] = 'O';
-            currentPlayer = player1;
-        }
-        zero.textContent = gameBoard[0]
-    });
-    const one = document.querySelector(".one");
-    one.addEventListener('click', () => {
-        console.log(currentPlayer);
-        if(currentPlayer == player1){
-            gameBoard[1] = 'X';
-            currentPlayer = player2;
-        }
-        else{
-            gameBoard[1] = 'O';
-            currentPlayer = player1;
-        }
-        one.textContent = gameBoard[1]
-    });
-    const two = document.querySelector(".two");
-    two.addEventListener('click', () => {
-        console.log(currentPlayer);
-        if(currentPlayer == player1){
-            gameBoard[2] = 'X';
-            currentPlayer = player2;
-        }
-        else{
-            gameBoard[2] = 'O';
-            currentPlayer = player1;
-        }
-        two.textContent = gameBoard[2]
-    });
-    
-}   
+let gameBoard = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
 
-Gameboard()
+const assign = document.querySelector(".submitBtn")
+
+assign.addEventListener('click', () =>{
+    const player1 =  document.getElementById("player1").value;
+    const player2 =  document.getElementById("player2").value;
+   
+})
+let currentPlayer = player1;
+
+const zero = document.querySelector(".zero");
+zero.addEventListener('click', () => {
+    let n = 0;
+    playedMove(n);
+    zero.textContent = gameBoard[0];
+    combination();
+});
+const one = document.querySelector(".one");
+one.addEventListener('click', () => {
+    let n = 1;
+    playedMove(n);
+    one.textContent = gameBoard[1];
+    combination();
+});
+const two = document.querySelector(".two");
+two.addEventListener('click', () => {
+    let n = 2;
+    playedMove(n);
+    two.textContent = gameBoard[2];
+    combination();
+});
+const three = document.querySelector(".three");
+three.addEventListener('click', () => {
+    let n = 3;
+    playedMove(n);
+    three.textContent = gameBoard[3];
+    combination();
+});
+const four = document.querySelector(".four");
+four.addEventListener('click', () => {
+    let n = 4;
+    playedMove(n);
+    four.textContent = gameBoard[4];
+    combination();
+});
+const five = document.querySelector(".five");
+five.addEventListener('click', () => {
+    let n = 5;
+    playedMove(n);
+    five.textContent = gameBoard[5];
+    combination();
+});
+const six = document.querySelector(".six");
+six.addEventListener('click', () => {
+    let n = 6;
+    playedMove(n);
+    six.textContent = gameBoard[6];
+    combination();
+});
+const seven = document.querySelector(".seven");
+seven.addEventListener('click', () => {
+    let n = 7;
+    playedMove(n);
+    seven.textContent = gameBoard[7];
+    combination();
+});
+const eight = document.querySelector(".eight");
+eight.addEventListener('click', () => {
+    let n = 8;
+    playedMove(n);
+    eight.textContent = gameBoard[8];
+    combination();
+});
+
+function playedMove(n)
+{
+    if(currentPlayer == player1){
+        gameBoard[n] = 'X';
+        currentPlayer =  player2;
+    }
+    else{
+        gameBoard[n] = 'O';
+        currentPlayer = player1;
+    }
+}
